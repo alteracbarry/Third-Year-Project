71323ee4375cad1e4fabcc90c3ae484d0df001fc
Yan <eeua86@bangor.ac.uk>
2012-01-31 01:28:40 +0000

    Major changes to Master.ino <forgot to stage>

--- Clock Sync/Master/Master.ino
+++ Clock Sync/Master/Master.ino
@@ -1,44 +1,71 @@
-// Script for Master Node
-// Waits for Hardware IRQ and sends semaphore "HIGH" which
-// starts code in the main loop
-#define BUTTON 2
+/* Script for Master Node :-
+   Implements a seperate ASCII-to-long capture method and uses
+   interrupts for interaction (no use of loop)
+   Areas for Improvement: Optimise, test, code for display instead of Serial
+*/
+#include <stdlib.h>
+#include <stdio.h>
+int Sensor = 4; // Ultrasonic Transducer at pin 4
+unsigned long RTTd2 = 0; // Half of the round-trip time; not necessarily a constant (ms)
+unsigned long Dist = 0; // Distance 
+long Toffset = 0; // Master and Slave clock difference (ms)
 
-volatile int Sem = LOW;
-unsigned long timer = 0;
-unsigned long incomingByte[4] = {0, 0, 0};
 
 void setup()
 {
-  attachInterrupt(0, detect, RISING);
+  attachInterrupt(0, Sync, RISING); // IRQ0 attached to pin 2 on Duemilanove
+  attachInterrupt(1, Pulse, RISING); // IRQ2 attached to pin 3 " "
   Serial.begin(9600);
-  pinMode(BUTTON, INPUT);
 }
 
 void loop()
 {
-  if(Sem == HIGH)
-  {
-    do
-    {
-      incomingByte[1] = Serial.read();
-    } while (incomingByte[1] == -1);
-    Serial.println(a2d(incomingByte[1]) - millis());
-  }
-  Serial.println(millis());
+  // Do Nothing
 }
 
-int a2d(byte c)
+void Sync()
 {
-    return c - '0';
-}
-
-
-void detect()
-{
-  Sem = HIGH;
   Serial.print("s");
-  Serial.print(millis());
+  unsigned long Tsend = millis(); // Take sent time
+  while (Serial.available() == 0)
+  {
+    // Loop Nothing until Serial arrives.
+  }
+  unsigned long Trec = millis(); // and receive time
+  RTTd2 = (Trec - Tsend)/2;
+  Toffset = capture() - RTTd2;
+  Serial.println("Sync DONE!");
 }
 
- 
+void Pulse()
+{
+  unsigned long Tpulse = millis(); // Timestamp pulse
+  tone(Sensor, 40000, 1); // 40 KHz transducer pulse for 1 ms
+  unsigned long Tsense = capture(); // Capture time of reception
+  unsigned long Tprop = (Tsense - (Tpulse + Toffset));
+  unsigned long Dist = (Tprop/3); // Speed of sound = ~330m/s = ~1/3 m/ms
+  Serial.print("Distance (m): ");
+  Serial.println(Dist);
+}
+
+long int capture()
+{
+  int k = 0;
+  int i = 0;
+  long int lin = 0;
+  char buffer[] = "";
+   while(Serial.available() > 0)
+   {
+     buffer[0 + i] = Serial.read();
+     delay(10); // cannot remove!!!
+     k = 1;
+     i++;
+   }
+   if(k == 1)
+   {
+     lin = strtol(buffer,NULL,10);
+   }
+   return lin;
+}
+
  
